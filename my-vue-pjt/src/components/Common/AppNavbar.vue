e0e16c6c9aa34dcdcd0688dc793156c9a9c637ec
최연지 <rokmc3038@naver.com>
2024-11-15 16:35:28 +0900

    수정

--- my-vue-pjt/src/components/Common/AppNavbar.vue
+++ my-vue-pjt/src/components/Common/AppNavbar.vue
@@ -10,19 +10,3 @@ import { RouterLink } from 'vue-router'
   </nav>
 </template>
 
-<style scoped>
-nav {
-  position: fixed;  /* 네비게이션 바를 화면에 고정 */
-  top: 0;           /* 상단에 배치 */
-  left: 0;          /* 왼쪽에 배치 */
-  width: 100%;      /* 가로 너비를 100%로 설정 */
-  background-color: white;  /* 배경색을 설정하여 글씨가 잘 보이도록 */
-  padding: 10px 20px;       /* 여백을 설정하여 보기 좋게 */
-  z-index: 1000;    /* 다른 요소 위에 표시되도록 설정 */
-  box-shadow: 0 4px 2px -2px gray; /* 그림자 효과로 구분감 추가 */
-}
-
-nav a {
-  margin-right: 20px; /* 링크 간 간격 */
-}
-</style>
