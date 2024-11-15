b911478cdb478ec0ab7aaed0e620c70672ec9d73
최연지 <rokmc3038@naver.com>
2024-11-15 15:52:51 +0900

    수정

--- my-vue-pjt/src/router/index.js
+++ my-vue-pjt/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MovieDetailView from '@/views/MovieDetailView.vue'
+import MovieListView from '@/views/MovieListView.vue'
+import RecommendedView from '@/views/RecommendedView.vue'
+import ReviewSearchView from '@/views/ReviewSearchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ my-vue-pjt/src/views/RecommendedView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>영화목록</h1>
+
+</template>
+
+<style scoped>
+
+</style>
